5172007738e851e1d97f5edc5242e8dec92e55d4
ozlem <ozlemtonkal@gmail.com>
2025-10-20 13:01:28 +0300

    Initial Commit

---
+++ PhotoGroupingAppTests/PhotoGroupingAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoGroupingAppTests.swift
+//  PhotoGroupingAppTests
+//
+//  Created by özlem tonkal on 20.10.2025.
+//
+
+import Testing
+@testable import PhotoGroupingApp
+
+struct PhotoGroupingAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
